4488cb9ec816ce4c4a8ea43e3cd93982e2cad775
Amit <singhamit.iot@gmail.com>
2022-11-29 23:31:42 +0530

    first commit

---
+++ Age_Calculator.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+def find_age(year, month, day):
+    today = datetime.date.today()
+    dob = datetime.date(year, month, day)
+    age = int((today - dob).days/365)
+    return age
+
+
+user_dob = input("Enter your D.O.B. in format of day-month-year: ")
+
+dob = user_dob.split('-')
+year, month, day = int(dob[0]), int(dob[1]), int(dob[2])
+your_age = find_age(year, month, day)
+print(f"you are {your_age} years old.")
