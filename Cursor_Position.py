94e90f4808cf7d89d4c781da0d9b71ec58acd4e8
Amit <singhamit.iot@gmail.com>
2022-11-30 17:56:09 +0530

    intial commit

--- Cursor_Position.py
+++ Cursor_Position.py
@@ -1,1 +1,17 @@
-
+while  1:
+        response = requests.get(IG_CFR_PAGE, headers=headers(""), params=params, cookies=cookies)
+        if response.status_code == 200:
+            cfr = response.json()
+            for entry in cfr["data"]["data"]:
+                #print(entry["text"])
+                usernames.append(entry["text"])
+            if cfr["data"]["cursor"]!= None:
+                params['cursor'] = cfr["data"]["cursor"]
+                time.sleep(1)
+            else:
+                break
+        else:
+           print(response.status_code)
+           print("Error in request .... aborting")
+           break
+print("ok")
---
+++ electricity_bill.py
@@ -0,0 +1,24 @@
+unit = float(input("Enter electricity meter measurement in unit: "))
+
+un = unit
+Rs = 0
+total = 0
+
+if un <= 50:
+    Rs = (un * 50/100)
+    total = (Rs + (Rs * 2/100))
+
+elif (un > 50) and (un <= 100):
+    Rs = (un * 1)
+    total = (Rs + (Rs * 2/100))
+
+elif (un > 100) and (un <= 250):
+    Rs = (un * 150/100)
+    total = (Rs + (Rs * 2/100))
+
+elif un > 250:
+    Rs = (un * 2)
+    total = (Rs + (Rs * 3/100))
+
+print(f"Electricity Bill Without Charge is = {float(Rs)} ")
+print(f"Electricity Bill With Charge is = {float(total)}")
